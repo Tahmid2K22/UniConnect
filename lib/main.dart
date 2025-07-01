768b4a07d45a6304df703e84f4f03bb3621bb3d1
IsaacAneek <81284474+IsaacAneek@users.noreply.github.com>
2025-07-02 04:47:38 +0600

    init firebase

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,9 @@ import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+  );
   await Hive.initFlutter();
   Hive.registerAdapter(TodoTaskAdapter());
   await Hive.openBox<TodoTask>('todoBox');
